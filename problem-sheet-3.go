38908378a25dd0c2de824fa6ff4dceb5a16b91a1
DamianGavin <10007969@gmit.ie>
2017-11-11 11:37:40 +0000

    part1

--- problem-sheet-3.go
+++ problem-sheet-3.go
@@ -1,7 +1,22 @@
+//Problem sheet 3 Data rep & query
+//Damian Gavin 11/11/17
+
 package main
 
-//"./eliza"
+import (
+	"math/rand"
+)
 
 func main() {
 
+}
+
+var responseStrings = []string{ //hardcode given responses
+	"I’m not sure what you’re trying to say. Could you explain it to me?",
+	"How does that make you feel?",
+	"Why do you say that?",
+}
+
+func ElizaResponse(input string) string {
+	return responseStrings[rand.Intn(len(responseStrings))]
 }
